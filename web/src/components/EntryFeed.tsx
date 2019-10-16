118fbb82cc3ff1718fca685f25a637a0bdabdc76
DeathstarNovember <johnathon.hill@gmail.com>
2019-10-16 01:27:49 -0400

    Add feed filter button

--- web/src/components/EntryFeed.tsx
+++ web/src/components/EntryFeed.tsx
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import gql from "graphql-tag";
 import { useQuery } from "@apollo/react-hooks";
-import { formatTimeMS } from "../util";
 import { User, Entry } from "../types";
 import EntryPreview from "./EntryPreview";
 
@@ -21,11 +20,18 @@ const LIST_ENTRIES = gql`
     }
   }
 `;
+
 type EntryFeedProps = {
   currentUser?: User;
 };
+
 const EntryFeed: React.FC<EntryFeedProps> = ({ currentUser }) => {
   const { loading, error, data } = useQuery(LIST_ENTRIES);
+  enum DisplayOptions {
+    Public,
+    Personal,
+  }
+  const [displayOption, setDisplayOption] = useState(DisplayOptions.Public);
   if (loading) {
     return <div className="p-6 rounded-lg shadow-xl">Loading....</div>;
   }
@@ -48,11 +54,39 @@ const EntryFeed: React.FC<EntryFeedProps> = ({ currentUser }) => {
       </div>
     );
   }
+
+  const publicEntries: Entry[] = data.listEntries;
+  const myEntries: Entry[] = currentUser
+    ? data.listEntries.filter((e: Entry) => e.user.id === currentUser.id)
+    : undefined;
+  const entryList =
+    displayOption === DisplayOptions.Personal ? myEntries : publicEntries;
+  const changeDisplayOption = (option: DisplayOptions) => {
+    setDisplayOption(option);
+  };
   return (
     <div className={"max-w-md mx-auto p-6"}>
       <div className="w-full max-w-md">
+        {displayOption !== DisplayOptions.Personal ? (
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mb-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+            onClick={() => changeDisplayOption(DisplayOptions.Personal)}
+          >
+            See Personal Feed
+          </button>
+        ) : null}
+        {displayOption !== DisplayOptions.Public && currentUser ? (
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mb-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+            onClick={() => changeDisplayOption(DisplayOptions.Public)}
+          >
+            See Public Feed
+          </button>
+        ) : null}
         <div className="p-6 bg-white rounded-lg shadow-xl">
-          {data.listEntries.map((entry: Entry, entryId: number) => (
+          {entryList.map((entry: Entry, entryId: number) => (
             <EntryPreview
               key={`${entry.user.id}${entryId}`}
               entry={entry}
