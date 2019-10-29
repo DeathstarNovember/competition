83e03e85ec6a5fd22cdb96f75541446d9e537dd5
DeathstarNovember <johnathon.hill@gmail.com>
2019-10-29 09:53:31 -0400

    Remove CORS

--- config/config.exs
+++ config/config.exs
@@ -22,6 +22,11 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+# config :cors_plug,
+#   origin: ["http://example.com"],
+#   max_age: 50000,
+#   methods: ["GET", "POST"]
+
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
